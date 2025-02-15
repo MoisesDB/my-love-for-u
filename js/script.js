e415ad23ef95429343b13772c57896fe6f639ef4
MoisesDB <moises_dalbosco@hotmail.com>
2025-02-14 23:33:08 -0300

    layout

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function updateTimeElapsed() {
-    const startDate = new Date("2025-01-14T00:00:00"); // Data inicial
-    const now = new Date(); // Data atual
-    let diff = Math.abs(now - startDate) / 1000; // Diferença em segundos
+    const startDate = new Date("2025-01-14T00:00:00");
+    const now = new Date();
+    let diff = Math.abs(now - startDate) / 1000;
 
     const days = Math.floor(diff / (3600 * 24));
     diff -= days * 3600 * 24;
@@ -18,5 +18,39 @@ function updateTimeElapsed() {
         `${days} dias, ${hours} horas, ${minutes} minutos, ${seconds} segundos`;
 }
 
-setInterval(updateTimeElapsed, 1000); // Atualiza a cada segundo
-updateTimeElapsed(); // Chama a função imediatamente ao carregar
+particlesJS("particles-js", {
+    "particles": {
+        "number": {
+            "value": 100, 
+            "density": { "enable": true, "value_area": 800 }
+        },
+        "shape": {
+            "type": "image", 
+            "image": {
+                "src": "https://cdn-icons-png.flaticon.com/128/833/833472.png", 
+                "width": 100,
+                "height": 100
+            }
+        },
+        "line_linked": {
+            "enable": false
+        },
+        "size": {
+            "value": 15,
+            "random": true
+        },
+        "move": {
+            "enable": true,
+            "speed": 2,
+            "direction": "top",
+            "out_mode": "out"
+        },
+        "opacity": {
+            "value": 0.7,
+            "random": true
+        }
+    }
+});
+
+setInterval(updateTimeElapsed, 1000);
+updateTimeElapsed();
